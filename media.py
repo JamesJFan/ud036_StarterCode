dfecca633bc2376f669d9973eda3a728ed2bd3f5
JamesJFan <james.j.fan@gmail.com>
2017-06-09 22:52:15 -0400

    Add files via upload

--- media.py
+++ media.py
@@ -1,13 +1,15 @@
 import webbrowser
 
-class Movie():
+
+class Movie:
     """This class is used to store movie related information."""
 
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    VALID_RATINGS = [ "G", "PG", "PG-13", "R"]
     # self doesn't have to be self, but it is a convention that is used
     # constructor for class Movie, and the arguments that it takes
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -16,4 +18,4 @@ class Movie():
     # this function/method will open a trailer for a movie
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
+
